c1ade09be3c6abd61955ce6802562656b857ac23
Gyujeong-Lee <rbwjd0101@naver.com>
2021-05-07 20:28:11 +0900

    game1_in_progress

--- game/views.py
+++ game/views.py
@@ -3,7 +3,16 @@ from ssafy5_3.models import Student
 
 def stage1(request):
     students = Student.objects.all()
-    
+    # nicknames = {
+    #     '이규정': '꽃규정',
+    #     '한상길': '데스파시토',
+    #     '조혜인': '조조교',
+    #     '이다영': 'MD장인',
+    #     '권오우': '오우마이걸',
+    #     '황지원': '갓덕삼',
+    #     '김주현': '반장drop',
+    #     ''
+    # }
     return render(request, 'game/stage1.html')
 
 
