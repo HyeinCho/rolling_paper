48f1f8d226bd19805caa73f4481cd66ef1d9ae39
Gyujeong-Lee <rbwjd0101@naver.com>
2021-05-08 20:38:26 +0900

    script code 초안

--- game/views.py
+++ game/views.py
@@ -8,7 +8,7 @@ from random import randint
 
     # nicknames = ['꽃규정', '데스파시토', '조조교', 'MD장인', '오우마이걸', '홈카페사장', '갓덕삼', '점심시간딜러', '반장drop']
     # names = ['이규정', '한상길', '조혜인', '이다영', '권오우', '황지원', '김주현', '이민아', '조규태']
-    
+
 def stage1(request):
     #모든 학생들 중 해당 스테이지에서 언급되는 학생들 flag를 통해 구분하기
     students = Student.objects.all()
@@ -19,16 +19,17 @@ def stage1(request):
 
     for student in students:
         if student.name in names:
-            student.flag = False
-            
-    #save를 해야하는가?
-    context = {
-        #닉네임 출력
-        'nicknames':nicknames,
-        #확인용
-        'students':students,
-    }
-    return render(request, 'game/stage1.html', context)
+            student.flag = False       
+
+    if request.is_ajax():
+        data = serializers.serialize('json', nicknames)
+        return HttpResponse(data, content_type='application/json')
+    else:
+        context = {
+            'nicknames':nicknames,
+        }
+        return render(request, 'game/stage1.html', context)
+    
 
 def stage2(request):
     if not Card.objects.all(): # 나중에 포스트로 바꿀거임
