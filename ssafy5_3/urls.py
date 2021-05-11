efb65efad15f92910d454c21c446b8afd8d2f6dc
HyeinCho <chohyein96@gmail.com>
2021-05-11 21:53:46 +0900

    아침 조례 페이지 완성

--- ssafy5_3/urls.py
+++ ssafy5_3/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'ssafy5_3'
 urlpatterns = [
-    path('', views.index, name='ssafy5_3.index'),
-    path('messages/', views.messages, name='ssafy5_3.messages'),
-    path('collegues/', views.collegues, name='ssafy5_3.collegues'),
-    path('greetings/', views.messages, name='ssafy5_3.greetings'),
+    path('', views.index, name='index'),
+    path('messages/', views.messages, name='messages'),
+    path('collegues/', views.collegues, name='collegues'),
+    path('greetings/', views.greetings, name='greetings'),
 ]
--- ssafy5_3/views.py
+++ ssafy5_3/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
+from django.http import JsonResponse
 from .models import Student, Professor, Greeting, Comment
 
 def index(request):
@@ -15,11 +16,33 @@ def messages(request):
 
 def collegues(request):
     students = Student.objects.all()
+    juan = Professor.objects.get(pk=1)
     context = {
         'students': students,
+        'juan': juan,
     }
     return render(request, 'ssafy5_3/collegues.html', context)
 
 
 def greetings(request):
-    pass
+    greetings = get_list_or_404(Greeting)
+    serialized_greetings = []
+    for greeting in greetings:
+        tmp = greeting.content.split('\n\n')
+        greetings_content_br = []
+        for s in tmp:
+            greetings_content_br.extend(s.split('\n'))
+            greetings_content_br.append(' ')
+        greetings_content_br.pop()
+        
+        serialized_greetings.append({
+            'id': greeting.pk,
+            'content': greetings_content_br,
+            'created_at': greeting.created_at,
+        })
+    juan = get_object_or_404(Professor, pk=1)
+    context = {
+        'juan': juan,
+        'greetings': serialized_greetings,
+    }
+    return render(request, 'ssafy5_3/greetings.html', context)
